d382540c791a5490ea31de9f4f6991c71fa61840
Vibhor Sharma <krityamkarma858041@gmail.com>
2022-05-17 23:27:38 +0530

    first commit

---
+++ public/js/store.js
@@ -0,0 +1,67 @@
+import * as constants from "./constants.js"
+let state = {
+  socketId: null,
+  localStream: null,
+  remoteStream: null,
+  screenSharingActive: false,
+  screenSharingStream: null,
+  allowConnectionsFromStrangers: false,
+  callState : constants.callstate.CALL_AVAILABLE_ONLY_CHAT
+};
+
+export const setSocketId = (socketId) => {
+  state = {
+    ...state,
+    socketId,
+  };
+  console.log(state);
+};
+
+export const setLocalStream = (stream) => {
+  state = {
+    ...state,
+    localStream: stream,
+  };
+};
+
+export const setAllowConnectionsFromStrangers = (allowConnection) => {
+  state = {
+    ...state,
+    allowConnectionsFromStrangers: allowConnection,
+  };
+};
+
+export const setScreenSharingActive = (screenSharingActive) => {
+  state = {
+    ...state,
+    screenSharingActive,
+  };
+};
+
+export const setScreenSharingStream = (stream) => {
+  state = {
+    ...state,
+    screenSharingStream: stream,
+  };
+};
+
+export const setRemoteStream = (stream) => {
+  state = {
+    ...state,
+    remoteStream: stream,
+  };
+};
+
+export const setCallState = (callState)=>{
+  state = {
+    ...state,
+    callState:callState
+  }
+}
+
+export const getState = () => {
+  return state;
+};
+
+
+ 
